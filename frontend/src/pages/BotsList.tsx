73f3915f9c6c338ef31036fe5a8d8fe27bbe8360
Moji Ahmadi <Mo.ahmadi@outlook.com>
2025-12-19 16:07:20 +0000

    Frontend: Add the first version of BotsList

---
+++ frontend/src/pages/BotsList.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+export type BotStatus = "DISABLED" | "ENABLED" | "PAUSED";
+
+export type Bot = {
+  id: string;
+  name: string;
+  description?: string;
+  status: BotStatus;
+  created: number; //Epoc timestamp
+};
+export default function BotsList() {
+  const [bots, setBots] = useState<Bot[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const fetchBots = async () => {
+      try {
+        const result = await fetch("http://localhost:3000/bots");
+        if (!result.ok) {
+          throw new Error(`Response status: ${result.status}`);
+        }
+        setBots(await result.json());
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBots();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (bots.length === 0) return <div>No Bots found</div>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Bot list</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bots.map((bot) => (
+          <tr key={bot.id}>
+            <td>{bot.name}</td>
+            <td>{bot.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
