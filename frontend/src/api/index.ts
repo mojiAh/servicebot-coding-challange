3c46a393812dad14c7927f92d3d7881afa87dcd5
Moji Ahmadi <Mo.ahmadi@outlook.com>
2025-12-20 13:01:42 +0000

    Refactor: Implement caching in fetchJson utility to optimize data fetching

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,8 +1,15 @@
 const BASE_URL = "http://localhost:3000";
 
+const simpleCache = new Map<string, any>();
+
 export async function fetchJson<T>(url: string): Promise<T> {
+  if (simpleCache.has(url)) return simpleCache.get(url);
+
   const result = await fetch(`${BASE_URL + url}`);
   if (!result.ok) throw new Error(`Response status: ${result.status}`);
   const data = await result.json();
+
+  simpleCache.set(url, data);
+
   return data;
 }
